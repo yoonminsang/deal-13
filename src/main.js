678cf14a5ddf8fb2017b08b3892b9bb005d81bd7
yoonminsang <apdlyooapdl@naver.com>
2021-07-13 00:17:09 +0900

    [#4] wip: ⚙️ 서버 메인 코드 작성
    - passport, sequelize 미적용
    - 라우팅 기본세팅 주석처리
    - 에러처리 status와 json으로

---
+++ src/main.js
@@ -0,0 +1,84 @@
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import path from 'path';
+import cors from 'cors';
+import passport from 'passport';
+// import { sequelize } from './models/index.js';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// import passportConfig from './passport';
+// import indexRouter from './routes/index';
+// import authRouter from './routes/auth';
+
+dotenv.config();
+
+// sequelize
+//   .sync({ force: false })
+//   .then(() => {
+//     console.log('데이터베이스 연결 성공');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+const corsOption = {
+  origin: 'http://localhost:9000/',
+};
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// passportConfig();
+
+app.use(cors(corsOption));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24, // 1일
+    },
+    name: 'baemin-cookie',
+  }),
+);
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.use('/', indexRouter);
+// app.use('/auth', authRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  return res.status(err.status || 500).json(err);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
