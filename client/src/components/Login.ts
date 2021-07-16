d9e11e4c493bb551db3553f5238dbbe071d058e8
yoonminsang <apdlyooapdl@naver.com>
2021-07-16 10:32:41 +0900

    [#9] wip: :factor: 로그인 기능 구현
    - fetch로 통신
    - 유효성 검사

--- client/src/components/Login.ts
+++ client/src/components/Login.ts
@@ -1,12 +1,85 @@
-function Login({ app, user, go, back }) {
-  const target = document.createElement('div');
-  target.className = 'login slidein';
-  target.innerHTML = `login`;
-  target.addEventListener('click', back);
+import '../styles/Login.scss';
+function Login({ app, user, go, back, authProcess }) {
+  const $target = document.createElement('div');
+  $target.className = 'login slidein auth';
+  $target.innerHTML = `
+  <div class="top-bar off-white">
+    <div>
+      <div class="js-back icon icon-left"></div>
+    </div>
+    <div>
+      <div class="top-bar__text">로그인</div>
+    </div>
+    <div></div>
+  </div>
+  <div class="login-inner auth-inner">
+    <form class="js-form">
+      <input
+      type="text"
+      class="js-id input-large"
+      placeholder="아이디를 입력하세요."
+      maxLength="20"
+      />
+      <input
+      type="password"
+      class="js-password input-large"
+      placeholder="비밀번호를 입력하세요."
+      maxLength="20"
+      />
+      <button class="btn-large" type="submit">로그인</button>
+      <button class="js-signup render btn-transparent" type="button">회원가입</button>
+    </form>
+  </div>
+  `;
+  const $form = $target.querySelector('.js-form');
+  const $id: HTMLInputElement = $form.querySelector('.js-id');
+  const $password: HTMLInputElement = $form.querySelector('.js-password');
+  $target.addEventListener('click', (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+    const classList = target.classList;
+    if (classList.contains('js-back')) {
+      back();
+    } else if (classList.contains('render')) {
+      console.log(classList[0].slice(3));
+      go(classList[0].slice(3));
+    }
+  });
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = $id.value;
+    const password = $password.value;
+    if (!id) alert('아이디를 입력하세요!!');
+    else if (!password) alert('비밀번호를 입력하세요!!');
+    else
+      fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id,
+          password,
+        }),
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then(({ user, text }) => {
+          if (user) {
+            authProcess();
+            back();
+          } else {
+            if (text) alert(text);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+  });
   this.state = user;
   this.render = () => {
-    target.classList.replace('slideout', 'slidein');
-    app.appendChild(target);
+    $target.classList.replace('slideout', 'slidein');
+    app.appendChild($target);
   };
 }
 export default Login;
