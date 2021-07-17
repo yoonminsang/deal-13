496f4056baac9e67ca4913c41a50a98b599d3c2d
yoonminsang <apdlyooapdl@naver.com>
2021-07-17 16:40:38 +0900

    [#49] refacto: :hammer: 서버 에러메세지와 text 구분
    - 이전 커밋에서 설명

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const { user } = req;
   if (user) return res.json({ user });
-  return res.status(401).json({ user: null, text: '자동 로그인 실패' });
+  return res.status(401).json({ user: null, error: '자동 로그인 실패' });
 });
 
 router.post('/signup', async (req, res, next) => {
@@ -29,7 +29,7 @@ router.post('/signup', async (req, res, next) => {
     `SELECT EXISTS (SELECT * FROM users WHERE id='${id}') as exist`,
   );
   if (existId.exist)
-    return res.status(409).json({ text: '아이디가 존재합니다' });
+    return res.status(409).json({ error: '아이디가 존재합니다' });
   const [[regions]] = await db.query(
     `SELECT id FROM regions WHERE region='${region}' LIMIT 1`,
   );
@@ -50,7 +50,7 @@ router.post('/login', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) return next(err);
     if (!user)
-      return res.status(409).json({ text: '아이디 또는 비밀번호가 틀립니다' });
+      return res.status(409).json({ error: '아이디 또는 비밀번호가 틀립니다' });
     req.logIn(user, (err) => {
       if (err) return next(err);
       return res.redirect('/api/auth');
