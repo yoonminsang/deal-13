890683b8a88cd4885f6ab0d0985b2c2bef0dd9f5
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-20 01:41:25 +0900

    [#18] wip: :gear: 쿼리, goodsQuery 수정
    - 파라미터 변경에 따른 쿼리 변경
    - 상세 글 요청 시 조회수 증가
       - 정상 요청, 작성자와 아이디가 다를 경우
    - wish 관련 임시 주석 추가

--- server/src/queries/goods.query.js
+++ server/src/queries/goods.query.js
@@ -5,66 +5,100 @@ const insertGoods = async ({
   content,
   categoryId,
   price,
-  thumbnailId,
+  thumbnail,
   regionId,
   userId,
   urls,
 }) => {
   const result = await db.query(
-    `INSERT INTO goods(title, content, region_id, category_id, thumbnail_id, price, user_id) VALUES('${title}', '${content}', ${regionId}, ${categoryId}, ${thumbnailId}, ${price}, '${userId}')`,
+    `INSERT INTO goods(title, content, region_id, category_id, thumbnail, price, user_id) VALUES('${title}', '${content}', ${regionId}, ${categoryId}, ${thumbnail}, ${price}, '${userId}')`,
   );
   if (result) {
     const insertId = result[0].insertId;
-
-    return result;
-  } else return null;
+    const urlQuery = urls.map((url) => `(${url}, ${insertId})`);
+    const photoInsertResult = await db.query(
+      `INSERT INTO goods_photo(url, goods_id) VALUES${urlQuery.join(',')}`,
+    );
+    if (photoInsertResult) {
+      return true;
+    }
+  }
+  return null;
 };
 
-const selectGoods = async (regionId, categoryId) => {
+const selectGoods = async (regionId, categoryId, lastIndex) => {
   const result = await db.query(
-    `SELECT * FROM goods WHERE 
-    region_id = ${regionId}
-    AND 
-    category_id = ${
+    `
+    SELECT g.* FROM goods g
+    JOIN goods g2 
+    WHERE state = 0
+    AND regionId = ${regionId === Infinity ? '0 or region_id > -1' : regionId}
+    AND category_id = ${
       categoryId === Infinity ? '0 or category_id > -1' : categoryId
     }
-    ORDER BY created DESC
+    AND g2.id = ${lastIndex} 
+    AND g2.updated < g.updated
+    ORDER BY updated DESC
   `,
   );
-  if (result.length) return result;
-  else return null;
-};
-
-const selectGoodsByUserId = async (userId) => {
-  const result = await db.query(
-    `SELECT * FROM goods WHERE user_id = '${userId}' ORDER BY created DESC`,
-  );
-
-  if (result.length) return result;
-  else return null;
-};
-
-const selectGoodsDetailByGoodsId = async (goodsId) => {
-  const result = await db.query(
-    `SELECT g.*, CONCAT('[', GROUP_CONCAT(p.url), ']') AS urls, r.region, c.name as category FROM goods g, goods_photo p, regions r, category c WHERE g.id = ${goodsId} AND g.id = p.goods_id AND g.region_id = r.id AND g.category_id = c.id`,
-  );
-  console.log(result[0]);
   if (result.length) return result[0];
   else return null;
 };
 
+const selectGoodsByUserId = async (userId, lastIndex) => {
+  const result = await db.query(
+    `SELECT g.* FROM goods g
+    JOIN goods g2 
+    WHERE state = 0
+    AND g2.id = ${lastIndex} 
+    AND g2.updated < g.updated AND user_id = '${userId}' ORDER BY updated DESC`,
+  );
+
+  if (result.length) return result[0];
+  return null;
+};
+
+// wish...
+// const selectGoodsByUserId = async (userId, lastIndex) => {
+//   const result = await db.query(
+//     `SELECT g.* FROM goods g
+//     JOIN goods g2
+//     WHERE g2.id = ${lastIndex}
+//     AND g2.updated < g.updated AND user_id = '${userId}' ORDER BY updated DESC`,
+//   );
+
+//   if (result.length) return result[0];
+//   return null;
+// };
+
+const selectGoodsDetailByGoodsId = async (goodsId, userId) => {
+  const result = await db.query(
+    `SELECT g.*, CONCAT('[', GROUP_CONCAT(p.url), ']') AS urls, r.region, c.name as category FROM goods g, goods_photo p, region r, category c WHERE g.id = ${goodsId} AND g.state = 0 AND g.id = p.goods_id AND g.region_id = r.id AND g.category_id = c.id`,
+  );
+  if (result.length) {
+    if (result[0][0].user_id !== userId) {
+      await db.query(
+        `UPDATE goods SET view_count = view_count + 1 WHERE id = ${result[0][0].id} AND state = 0`,
+      );
+      result[0][0].view_count += 1;
+    }
+    return result[0];
+  }
+  return null;
+};
+
 const updateGoods = async ({
   id,
+  userId,
   title,
   content,
-  categoryId,
   price,
-  thumbnailId,
+  categoryId,
+  thumbnail,
   regionId,
-  userId,
 }) => {
   const result = await db.query(
-    `UPDATE goods SET title = '${title}', content = '${content}', category_id = ${categoryId}, price = ${price}, thumbnail_id = ${thumbnailId}, region_id = ${regionId}, WHERE id = ${id}, AND user_id = '${userId}'`,
+    `UPDATE goods SET title = '${title}', content = '${content}', category_id = ${categoryId}, price = ${price}, thumbnail = ${thumbnail}, region_id = ${regionId}, updated = CURRENT_TIMESTAMP WHERE id = ${id} AND user_id = '${userId}'`,
   );
   if (result.length) return result;
   else return null;
@@ -78,9 +112,9 @@ const updateGoodsSaleState = async (goodsId, state) => {
   else return null;
 };
 
-const updateGoodsViewState = async (goodsId, state) => {
+const deleteGoodsViewState = async (goodsId) => {
   const result = await db.query(
-    `UPDATE goods SET view_state = ${state} WHERE id = ${goodsId}`,
+    `UPDATE goods SET view_state = 1 WHERE id = ${goodsId}`,
   );
   if (result.length) return result;
   else return null;
@@ -93,5 +127,5 @@ export const goodsQuery = {
   selectGoodsDetailByGoodsId,
   updateGoods,
   updateGoodsSaleState,
-  updateGoodsViewState,
+  deleteGoodsViewState,
 };
