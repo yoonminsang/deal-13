a71afd5f298a62d83bc07171d3175c82c24662a9
kowoohyuk <kowoohyuk91@gmail.com>
2021-07-20 01:42:16 +0900

    [#18] wip: :gear: 라우터, goodsRouter 수정
    - controller 메소드 호출 방식 변경

--- server/src/routes/goods.js
+++ server/src/routes/goods.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { goodsPhotoController } from '../controllers/goods-photo.controller.js';
 import { goodsController } from '../controllers/goods.controller.js';
+import passport from 'passport';
 
 const router = express.Router();
 
@@ -12,13 +13,18 @@ const router = express.Router();
 // return res.status(401).json({ user: null, error: '자동 로그인 실패' });
 // });
 
-router.get('/', (req, res) => {
-  const { body, params } = req;
-  console.log(`body : ${body}`);
-  console.log(`params : ${params}`);
-});
+// router.get('/', (req, res) => {
+//   const { body, params } = req;
+//   console.log(`body : ${body}`);
+//   console.log(`params : ${params}`);
+//   console.log(req.user);
+//   console.log('1111');
+// });
 
 router.get('/:goodsId', goodsController.findGoodsDetailByGoodsId);
+router.delete('/:goodsId', goodsController.deleteGoodsViewState);
+router.put('/', goodsController.updateGoods);
+router.put('/:goodsId/:state', goodsController.updateGoodsSaleState);
 router.post('/', goodsController.createGoods);
 
 router.put('/', (req, res) => {
