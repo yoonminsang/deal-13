fda857c7002b1323b5cab6bbfe3823c0ebd33cdd
yoonminsang <apdlyooapdl@naver.com>
2021-07-20 22:25:34 +0900

    [#69] wip: :gear: 패스포트 regionid 추가

--- server/src/passport/index.js
+++ server/src/passport/index.js
@@ -10,11 +10,13 @@ const passportConfig = () => {
   });
   passport.deserializeUser(async (id, done) => {
     const [user] = await db.query(
-      `select distinct user.id, r.region from region_list list, region r, user where user.id = '${id}' and user.id = list.user_id and list.region_id = r.id`,
+      `select distinct user.id, r.region, r.id as region_id from region_list list, region r, user where user.id = '${id}' and user.id = list.user_id and list.region_id = r.id`,
     );
     if (user.length <= 2) {
       const region = user.map((v) => v.region);
       user[0].region = region;
+      const region_id = user.map((v) => v.region_id);
+      user[0].region_id = region_id;
       console.log('deserialize', user[0]);
     } else console.error('deserialize error! too many region');
     done(null, user[0]);
